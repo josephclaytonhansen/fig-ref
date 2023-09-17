1d6dac626424b921f764d021453748d5ac8e7b5a
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-09-16 22:54:37 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<?php
+//get URL param "length"
+if (!isset($_GET["length"])) {
+    $length = "short";
+} else {
+    $length = $_GET["length"];
+}
+$image_datas = [];
+$images_names = [];
+$images_30 = [];
+$images_60 = [];
+$images_300 = [];
+$images_600 = [];
+$images_900 = [];
+$starting_amounts = ["30", "60", "300", "600", "900"];
+
+function secondsToTime($seconds)
+{
+    $dtF = new \DateTime("@0");
+    $dtT = new \DateTime("@$seconds");
+    return $dtF->diff($dtT)->format("%i:%s");
+}
+
+function addTrailingZeroToTime($time)
+{
+    $time = explode(":", $time);
+    if (count($time) == 1) {
+        return "00:0" . $time[0];
+    }
+    if (count($time) == 2) {
+        if (strlen($time[0]) == 1) {
+            $time[0] = "0" . $time[0];
+        }
+        if (strlen($time[1]) == 1) {
+            $time[1] = "0" . $time[1];
+        }
+        return $time[0] . ":" . $time[1];
+    }
+    return "00:00";
+}
+
+foreach (new DirectoryIterator("./img") as $file) {
+    if ($file->isDot()) {
+        continue;
+    }
+    $name = $file->getFilename();
+    $images_names[] = $name;
+}
+
+if ($length === "short") {
+    $amounts = [5, 4, 3];
+} elseif ($length === "medium") {
+    $amounts = [10, 5, 3];
+} elseif ($length === "long") {
+    $amounts = [10, 5, 5];
+}
+
+$random_keys = array_rand($images_names, $amounts[0]);
+$random_images = [];
+foreach ($random_keys as $key) {
+    $random_images[] = $images_names[$key];
+}
+foreach ($random_images as $image) {
+    $images_30[] =
+        "data:image/" .
+        pathinfo($name, PATHINFO_EXTENSION) .
+        ";base64," .
+        base64_encode(file_get_contents("./img/" . $image));
+}
+$random_keys = array_rand($images_names, $amounts[1]);
+$random_images = [];
+foreach ($random_keys as $key) {
+    $random_images[] = $images_names[$key];
+}
+foreach ($random_images as $image) {
+    $images_60[] =
+        "data:image/" .
+        pathinfo($name, PATHINFO_EXTENSION) .
+        ";base64," .
+        base64_encode(file_get_contents("./img/" . $image));
+}
+$random_keys = array_rand($images_names, $amounts[2]);
+$random_images = [];
+foreach ($random_keys as $key) {
+    $random_images[] = $images_names[$key];
+}
+foreach ($random_images as $image) {
+    $images_300[] =
+        "data:image/" .
+        pathinfo($name, PATHINFO_EXTENSION) .
+        ";base64," .
+        base64_encode(file_get_contents("./img/" . $image));
+}
+
+if ($length === "medium" || $length === "long") {
+    $images_600[] =
+        "data:image/" .
+        pathinfo($name, PATHINFO_EXTENSION) .
+        ";base64," .
+        base64_encode(
+            file_get_contents(
+                "./img/" . $images_names[array_rand($images_names, 1)]
+            )
+        );
+}
+if ($length === "long") {
+    $images_900[] =
+        "data:image/" .
+        pathinfo($name, PATHINFO_EXTENSION) .
+        ";base64," .
+        base64_encode(
+            file_get_contents(
+                "./img/" . $images_names[array_rand($images_names, 1)]
+            )
+        );
+}
+?>
+
+<body>
+    <script
+    src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+    crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('.card').click(function(){
+                console.log($(this).attr('id'));
+            });
+        });
+    </script>
+    <div class = "button-row">
+        <button onclick="window.location.href = 'index.php?length=short';">Short</button>
+        <button onclick="window.location.href = 'index.php?length=medium';">Medium</button>
+        <button onclick="window.location.href = 'index.php?length=long';">Long</button>
+    </div>
+    <div class = "card-row">
+    <?php
+    $i = 0;
+    $ii = 0;
+    foreach (
+        [$images_30, $images_60, $images_300, $images_600, $images_900]
+        as $c
+    ) {
+        foreach ($c as $data) {
+            echo '
+        <div class = "card" id = "' .
+                $i .
+                "-" .
+                $ii .
+                '">
+            <div class = "rect-img-container">
+                <img class = "rect-img" src="' .
+                $data .
+                '">
+            </div>
+            <div class = "card-content">
+                <div class = "timer">
+                    <div class = "cutout">
+                        <div class = "amount">' .
+                addTrailingZeroToTime(secondsToTime($starting_amounts[$i])) .
+                '</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        ';
+            $ii = $ii + 1;
+        }
+        $i = $i + 1;
+    }
+    ?>
+        
+    </div>
+</body>
+</html>
