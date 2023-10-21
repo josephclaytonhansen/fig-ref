9a02741f591b57fbfb7a8d2fc263005ce1066305
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-10-21 08:58:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,125 @@ $images_600 = array();
 $images_900 = array();
 $starting_amounts = array('30', '60', '300', '600', '900');
 
+//check if 'lock.txt' exists, and if so, use the JSON from it as the images
+if(file_exists('lock.txt')){
+    $lock_json = file_get_contents('lock.txt');
+    $lock_json = json_decode($lock_json, true);
+    $images_30 = explode('-=|@|=-', $lock_json['images_30']);
+    $images_60 = explode('-=|@|=-', $lock_json['images_60']);
+    $images_300 = explode('-=|@|=-', $lock_json['images_300']);
+    $images_600 = explode('-=|@|=-', $lock_json['images_600']);
+    $images_900 = explode('-=|@|=-', $lock_json['images_900']);
+    //if an array is empty, remove it
+    if(count($images_30) == 1 && $images_30[0] == ''){$images_30 = array();}
+    if(count($images_60) == 1 && $images_60[0] == ''){$images_60 = array();}
+    
+    
+} else {
+    foreach (new DirectoryIterator('./img') as $file) {
+        if($file->isDot()) continue;
+        $name = $file->getFilename();
+        $images_names[] = $name;
+    }
+    
+    if ($length === 'short') {
+        if ($balance === 'short') {
+            $amounts = [3, 4, 2];
+        } else if ($balance === 'balanced') {
+            $amounts = [5, 4, 3];
+        } else if ($balance === 'long') {
+            $amounts = [2, 2, 5];
+        }
+    } else if ($length === 'medium') {
+        if ($balance === 'short') {
+            $amounts = [7, 5, 3];
+        } else if ($balance === 'balanced') {
+            $amounts = [5, 6, 3];
+        } else if ($balance === 'long') {
+            $amounts = [4, 5, 4];
+        }
+    } else if ($length === 'long') {
+        if ($balance === 'short') {
+            $amounts = [7, 5, 3];
+        } else if ($balance === 'balanced') {
+            $amounts = [5, 6, 3];
+        } else if ($balance === 'long') {
+            $amounts = [4, 5, 4];
+        }
+    } else if ($length === 'long (no 30s)') {
+        if ($balance === 'short') {
+            $amounts = [0, 5, 3];
+        } else if ($balance === 'balanced') {
+            $amounts = [0, 6, 3];
+        } else if ($balance === 'long') {
+            $amounts = [0, 5, 4];
+        }
+    } else if ($length === 'medium (no 30s)'){
+        if ($balance === 'short') {
+            $amounts = [0, 5, 3];
+        } else if ($balance === 'balanced') {
+            $amounts = [0, 6, 3];
+        } else if ($balance === 'long') {
+            $amounts = [0, 5, 4];
+        }
+    } else if ($length === 'short (no 30s)'){
+        if ($balance === 'short') {
+            $amounts = [0, 4, 2];
+        } else if ($balance === 'balanced') {
+            $amounts = [0, 4, 3];
+        } else if ($balance === 'long') {
+            $amounts = [0, 2, 5];
+        }
+    } else if ($length === '>1m'){
+        if ($balance === 'short') {
+            $amounts = [0, 0, 2];
+        } else if ($balance === 'balanced') {
+            $amounts = [0, 0, 3];
+        } else if ($balance === 'long') {
+            $amounts = [0, 0, 5];
+        }
+    }
+    
+    if ($amounts[0] > 0){
+    $random_keys = array_rand($images_names, $amounts[0]);
+    $random_images = array();
+    foreach ($random_keys as $key) {
+        $random_images[] = $images_names[$key];
+    }
+    foreach ($random_images as $image) {
+        $images_30[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$image));
+    }
+    }
+    if ($amounts[1] > 0){
+    $random_keys = array_rand($images_names, $amounts[1]);
+    $random_images = array();
+    foreach ($random_keys as $key) {
+        $random_images[] = $images_names[$key];
+    }
+    foreach ($random_images as $image) {
+        $images_60[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$image));
+    }
+    }
+    $random_keys = array_rand($images_names, $amounts[2]);
+    $random_images = array();
+    foreach ($random_keys as $key) {
+        $random_images[] = $images_names[$key];
+    }
+    foreach ($random_images as $image) {
+        $images_300[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$image));
+    }
+    
+    if($length === 'medium' || $length === 'long' || $length === '>1m'){
+    $images_600[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$images_names[array_rand($images_names, 1)]));}
+    if ($length === 'medium (no 30s)' || $length === 'long (no 30s)') {
+        $images_600[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/' . $images_names[array_rand($images_names, 1)]));
+    }
+    if($length === 'long' || '>1m'){
+    $images_900[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$images_names[array_rand($images_names, 1)]));
+    }
+
+}
+
 function secondsToTime($seconds) {
     $dtF = new \DateTime('@0');
     $dtT = new \DateTime("@$seconds");
@@ -50,107 +169,6 @@ function addTrailingZeroToTime($time){
     return '00:00';
 }
 
-foreach (new DirectoryIterator('./img') as $file) {
-    if($file->isDot()) continue;
-    $name = $file->getFilename();
-    $images_names[] = $name;
-}
-
-if ($length === 'short') {
-    if ($balance === 'short') {
-        $amounts = [3, 4, 2];
-    } else if ($balance === 'balanced') {
-        $amounts = [5, 4, 3];
-    } else if ($balance === 'long') {
-        $amounts = [2, 2, 5];
-    }
-} else if ($length === 'medium') {
-    if ($balance === 'short') {
-        $amounts = [7, 5, 3];
-    } else if ($balance === 'balanced') {
-        $amounts = [5, 6, 3];
-    } else if ($balance === 'long') {
-        $amounts = [4, 5, 4];
-    }
-} else if ($length === 'long') {
-    if ($balance === 'short') {
-        $amounts = [7, 5, 3];
-    } else if ($balance === 'balanced') {
-        $amounts = [5, 6, 3];
-    } else if ($balance === 'long') {
-        $amounts = [4, 5, 4];
-    }
-} else if ($length === 'long (no 30s)') {
-    if ($balance === 'short') {
-        $amounts = [0, 5, 3];
-    } else if ($balance === 'balanced') {
-        $amounts = [0, 6, 3];
-    } else if ($balance === 'long') {
-        $amounts = [0, 5, 4];
-    }
-} else if ($length === 'medium (no 30s)'){
-    if ($balance === 'short') {
-        $amounts = [0, 5, 3];
-    } else if ($balance === 'balanced') {
-        $amounts = [0, 6, 3];
-    } else if ($balance === 'long') {
-        $amounts = [0, 5, 4];
-    }
-} else if ($length === 'short (no 30s)'){
-    if ($balance === 'short') {
-        $amounts = [0, 4, 2];
-    } else if ($balance === 'balanced') {
-        $amounts = [0, 4, 3];
-    } else if ($balance === 'long') {
-        $amounts = [0, 2, 5];
-    }
-} else if ($length === '>1m'){
-    if ($balance === 'short') {
-        $amounts = [0, 0, 2];
-    } else if ($balance === 'balanced') {
-        $amounts = [0, 0, 3];
-    } else if ($balance === 'long') {
-        $amounts = [0, 0, 5];
-    }
-}
-
-if ($amounts[0] > 0){
-$random_keys = array_rand($images_names, $amounts[0]);
-$random_images = array();
-foreach ($random_keys as $key) {
-    $random_images[] = $images_names[$key];
-}
-foreach ($random_images as $image) {
-    $images_30[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$image));
-}
-}
-if ($amounts[1] > 0){
-$random_keys = array_rand($images_names, $amounts[1]);
-$random_images = array();
-foreach ($random_keys as $key) {
-    $random_images[] = $images_names[$key];
-}
-foreach ($random_images as $image) {
-    $images_60[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$image));
-}
-}
-$random_keys = array_rand($images_names, $amounts[2]);
-$random_images = array();
-foreach ($random_keys as $key) {
-    $random_images[] = $images_names[$key];
-}
-foreach ($random_images as $image) {
-    $images_300[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$image));
-}
-
-if($length === 'medium' || $length === 'long' || $length === '>1m'){
-$images_600[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$images_names[array_rand($images_names, 1)]));}
-if ($length === 'medium (no 30s)' || $length === 'long (no 30s)') {
-    $images_600[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/' . $images_names[array_rand($images_names, 1)]));
-}
-if($length === 'long' || '>1m'){
-$images_900[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,' . base64_encode(file_get_contents('./img/'.$images_names[array_rand($images_names, 1)]));
-}
 ?>
 
 <body>
@@ -159,6 +177,48 @@ $images_900[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,'
     integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
     crossorigin="anonymous"></script>
     <script type="text/javascript">
+
+        function lockImages(){
+            var images = $('.rect-img');
+            var images_src = [];
+            var images_30 = [];
+            var images_60 = [];
+            var images_300 = [];
+            var images_600 = [];
+            var images_900 = [];
+
+            for(var i = 0; i < images.length; i++){
+                let image_time = $(images[i]).parent().parent().find('.amount').text();
+                let image_time_split = image_time.split(':');
+                let image_time_seconds = parseInt(image_time_split[0]) * 60 + parseInt(image_time_split[1]);
+                let image_src = $(images[i]).attr('src');
+
+                if(image_time_seconds == 30){images_30.push(image_src);}
+                if(image_time_seconds == 60){images_60.push(image_src);}
+                if(image_time_seconds == 300){images_300.push(image_src);}
+                if(image_time_seconds == 600){images_600.push(image_src);}
+                if(image_time_seconds == 900){images_900.push(image_src);}
+        }
+        //convert these arrays to JSON
+        let params = {
+            images_30: images_30.join('-=|@|=-'),
+            images_60: images_60.join('-=|@|=-'),
+            images_300: images_300.join('-=|@|=-'),
+            images_600: images_600.join('-=|@|=-'),
+            images_900: images_900.join('-=|@|=-')
+        };
+
+        console.log(params);
+
+        //save new_params as lock.json 
+        let fileContent = JSON.stringify(params);
+        var bb = new Blob([fileContent ], { type: 'text/plain' });
+        var a = document.createElement('a');
+        a.download = 'lock.txt';
+        a.href = window.URL.createObjectURL(bb);
+        a.click();
+        a.remove();
+    }
 
         function changeBalanceURLParam(balance){
             var url = window.location.href;
@@ -242,6 +302,7 @@ $images_900[] = 'data:image/' . pathinfo($name, PATHINFO_EXTENSION) . ';base64,'
         <button onclick="window.location.href = 'index.php?length=medium (no 30s)&balance=balanced';">Medium (no 30s)</button>
         <button onclick="window.location.href = 'index.php?length=long (no 30s)&balance=balanced';">Long (no 30s)</button>
         <button onclick="window.location.href = 'index.php?length=>1m&balance=balanced';">>1m</button>
+        <button id = "lock-button" onclick="lockImages()">Lock images</button>
     </div>
     <div class = "button-row">
         <button onclick="changeBalanceURLParam('short')">Veer short</button>
